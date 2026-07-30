a674cf311f79079bf86f1075a64490c9d19a1e86
fxfayad <fxfayad@gmail.com>
2026-07-30 15:33:13 +0600

    test

--- Testcode.py
+++ Testcode.py
@@ -1,5 +1,41 @@
-#creating a class
-class myClass:
-    x = 5
-p1 = myClass()
-print(p1.x)
+# #python practice after a long time 
+# # How a loop work 
+
+colors = ("red","green")
+for color in colors:
+    print(color) #print the color in colors
+    for i in color:
+        print(i)
+
+# #range 
+
+for i in range (20):
+    print (i) # zero to 19 
+    print(i+1) # 1 to 20
+
+for i in range (3):
+    print (i)
+
+# # input in python 
+
+var1 = int(input("enter your value :"))
+var2 = int(input("enter your value :"))
+
+sum = (var1 + var2)
+print(sum)
+
+score = 100
+if score >= 90:
+    print("Grade A")
+elif score >= 80:
+    print("Grade B")
+
+
+
+
+#  function 
+# a function is a block of code that run only when its called 
+
+def fahrenheit_to_celsious(fahrenheit): #define function 
+    return (fahrenheit -32) * 5/9 #actual function
+
